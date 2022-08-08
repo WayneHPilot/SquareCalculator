6e644ca887f53fa2fa07ec60cb4db07d14acda2c
Wayne Pilot <wharveypilot@gmail.com>
2022-08-08 16:01:32 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+    double height;
+    height = 25.3;
+    // declaring height value
+    double width;
+    width = 13.5;
+    // declaring width value
+    double perimeter;
+    perimeter = 2*(width+height);
+    // multiples the sum of width + height by 2
+    double area;
+    area = width*height;
+    // multiplies the width and height together
+
+        System.out.println(perimeter);
+        // prints the perimeter that has been calculated
+        System.out.println(area);
+        // prints the area value that has been calculated
+    }
+}
